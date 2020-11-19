4bca119b92e6e70bb97572d36b53767f55ca7890
Tim <74612255+Tim-Bolhoeve@users.noreply.github.com>
2020-11-19 15:36:44 +0100

    Test file toegevoegd
    Test file voor het testen van delen code voordat het de main file in gaat

---
+++ test.py
@@ -0,0 +1,3 @@
+#deze file is voor potentiele code en voor het testen van half werkende code of dingen waar we nog aan moeten werken
+#zonder dat we de main file aan hoeven te passen
+#als deze file leeg is, is er momenteel geen code die er getest wordt en werkt in principe alles
